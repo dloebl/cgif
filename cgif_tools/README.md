33abb4864e4d73238cfd02258ea80102b3cdb81e
Daniel Löbl <dloebl.2000@gmail.com>
2021-12-19 17:14:27 +0100

    switched to libnsgif

--- cgif_tools/README.md
+++ cgif_tools/README.md
@@ -4,10 +4,13 @@ gifresize is a simple tool (experimental) for fast GIF resizing using cgif.
 ## running gifresize
 ```sh
 cd cgif
-# init and update gifdec submodule
+# init and update libnsgif submodule
 git submodule init
 git submodule update
 cd cgif_tools
 make
 # ./gifresize <input-gif> <output-gif> <new-width> <new-height>
 ```
+
+## Notes
+- User-defined transparency is not yet supported.
--- cgif_tools/gifresize.c
+++ cgif_tools/gifresize.c
@@ -1,11 +1,34 @@
 #include <cgif.h>
-#include <gifdec.h>
+#include <libnsgif.h>
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdint.h>
 
 #define MULU16(a, b) (((uint32_t)a) * ((uint32_t)b)) // helper macro to correctly multiply two U16's without default signed int promotion
+
+/* callbacks for libnsgif */
+static void* cb_create(int width, int height) {
+  return malloc(width * height * 4);
+}
+
+static void cb_destroy(void* bitmap) {
+  free(bitmap);
+}
+
+static uint8_t* cb_get_buffer(void* bitmap) {
+  return (uint8_t*)bitmap;
+}
+
+/* helper function to get file size of open file in C99 */
+static uint64_t getFileSize(FILE* pFile) {
+  long curPos = ftell(pFile);
+  fseek(pFile, 0, SEEK_END);
+  long endPos = ftell(pFile);
+  fseek(pFile, curPos, SEEK_SET);
+  return endPos;
+}
 
 /* resize image without any color averaging and reusing (RGB) colors from before */
 static uint8_t* resize_naive_rgb(const uint8_t* pImageData, uint16_t width, uint16_t height, uint16_t width_new, uint16_t height_new){
@@ -17,9 +40,9 @@ static uint8_t* resize_naive_rgb(const uint8_t* pImageData, uint16_t width, uint
     for(uint16_t j = 0; j < width_new; ++j) {
       original_y = MULU16(height, i) / height_new;
       original_x = MULU16(width, j) / width_new;
-      pImgResized[MULU16(3 * i, width_new) + 3 * j]     = pImageData[MULU16(3 * original_y, width) + 3 * original_x];
-      pImgResized[MULU16(3 * i, width_new) + 3 * j + 1] = pImageData[MULU16(3 * original_y, width) + 3 * original_x + 1];
-      pImgResized[MULU16(3 * i, width_new) + 3 * j + 2] = pImageData[MULU16(3 * original_y, width) + 3 * original_x + 2];
+      pImgResized[MULU16(3 * i, width_new) + 3 * j]     = pImageData[MULU16(4 * original_y, width) + 4 * original_x];
+      pImgResized[MULU16(3 * i, width_new) + 3 * j + 1] = pImageData[MULU16(4 * original_y, width) + 4 * original_x + 1];
+      pImgResized[MULU16(3 * i, width_new) + 3 * j + 2] = pImageData[MULU16(4 * original_y, width) + 4 * original_x + 2];
     }
   }
   return pImgResized;
@@ -77,19 +100,40 @@ int main(int argn, char** pArgs) {
   char* sOutput = pArgs[2];
   uint16_t newWidth = atoi(pArgs[3]);
   uint16_t newHeight = atoi(pArgs[4]);
-  if(newWidth == 0 && newHeight == 0) {
+  if(newWidth == 0 || newHeight == 0) {
     fprintf(stderr, "%s\n", "invalid new width or new height");
     return 2; // error    
   }  
   //
-  // read in GIF: done by libnsgif in case of libvips
-  // TBD replace gifdec with libnsgif
-  gd_GIF* dGIF  = gd_open_gif(sInput);  
-  if(!dGIF) {
+  // read in GIF via libnsgif
+  FILE* pFile = fopen(sInput, "rb");
+  if(!pFile) {
     fprintf(stderr, "%s\n", "failed to open <input-gif>");
-    return 3;
+    return 3; // error
   }
-  uint8_t* pRGB = malloc(dGIF->width * dGIF->height * 3);
+  uint64_t fileSize = getFileSize(pFile);
+  uint8_t* pGIFData = malloc(fileSize);
+  size_t cnt = fread(pGIFData, 1, fileSize, pFile);
+  fclose(pFile);
+  if(cnt != fileSize) {
+    fprintf(stderr, "%s\n", "failed to read from <input-gif>");
+    free(pGIFData);
+    return 4; // error
+  }
+
+  // read GIF
+  gif_animation dGIF;
+  gif_bitmap_callback_vt bcb = { cb_create, cb_destroy, cb_get_buffer, NULL, NULL, NULL };
+  gif_create(&dGIF, &bcb);
+  gif_result r = gif_initialise(&dGIF, fileSize, pGIFData);
+  //
+  // check for errors
+  if(r != GIF_OK) {
+    fprintf(stderr, "%s\n", "<input-gif> is not a valid GIF");
+    free(pGIFData);
+    return 5;
+  }
+
   uint8_t* pImageData = malloc(MULU16(newWidth, newHeight));
   uint8_t* aPalette = malloc(256 * 3); // max size of palette
   //
@@ -99,31 +143,36 @@ int main(int argn, char** pArgs) {
   gConf.attrFlags        = CGIF_ATTR_IS_ANIMATED | CGIF_ATTR_NO_GLOBAL_TABLE;
   gConf.width            = newWidth;
   gConf.height           = newHeight;
+  gConf.numLoops         = dGIF.loop_count;
   CGIF* eGIF             = cgif_newgif(&gConf);
   CGIF_FrameConfig fConf = {0};
   fConf.pLocalPalette    = aPalette;
   fConf.pImageData       = pImageData;
   fConf.attrFlags        = CGIF_FRAME_ATTR_USE_LOCAL_TABLE;
   fConf.genFlags         = CGIF_FRAME_GEN_USE_TRANSPARENCY | CGIF_FRAME_GEN_USE_DIFF_WINDOW;
-  int r                  = gd_get_frame(dGIF);  
   //
   // encode frame by frame. generate one local color table per frame.
-  while(r) {
-    gd_render_frame(dGIF, pRGB); 
-    fConf.delay = dGIF->gce.delay;
-    uint8_t* pNewRGB = resize_naive_rgb(pRGB, dGIF->width, dGIF->height, newWidth, newHeight);   // resize RGB frame
-    int cnt          = rgb_to_index(pNewRGB, MULU16(newWidth, newHeight), pImageData, aPalette); // get local palette from RGB frame
-    fConf.numLocalPaletteEntries = cnt;
+  for(uint32_t i = 0; i < dGIF.frame_count; ++i) {
+    r = gif_decode_frame(&dGIF, i);
+    if(r != GIF_OK) break; // error occurred
+    fConf.delay = dGIF.frames[i].frame_delay;
+    uint8_t* pRGBA   = dGIF.frame_image;
+    uint8_t* pNewRGB = resize_naive_rgb(pRGBA, dGIF.width, dGIF.height, newWidth, newHeight);   // resize RGBA frame
+    int cntBlock  = rgb_to_index(pNewRGB, MULU16(newWidth, newHeight), pImageData, aPalette); // get local palette from RGB frame
+    fConf.numLocalPaletteEntries = cntBlock;
     cgif_addframe(eGIF, &fConf);
-    r = gd_get_frame(dGIF);
     free(pNewRGB);
   }
-  gd_close_gif(dGIF);
   cgif_close(eGIF);
+  gif_finalise(&dGIF);
   //
   // free stuff
-  free(pRGB);
   free(pImageData);
   free(aPalette);  
+  free(pGIFData);
+  if(r != GIF_OK) {
+    fprintf(stderr, "%s\n", "failed to decode frame");
+    return 6;
+  }
   return 0;
 }
