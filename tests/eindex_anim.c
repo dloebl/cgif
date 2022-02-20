08fc24d3929c7c50fd0fd1826d03dc13bf3cebe0
Daniel Löbl <dloebl.2000@gmail.com>
2022-02-20 16:26:03 +0100

    add test for potential double free

---
+++ tests/eindex_anim.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "cgif.h"
+
+#define WIDTH  100
+#define HEIGHT 100
+
+static int pWriteFn(void* pContext, const uint8_t* pData, const size_t numBytes) {
+  (void)pContext;
+  (void)pData;
+  (void)numBytes;
+  // just ignore GIF data
+  return 0;
+}
+
+int main(void) {
+  CGIF*          pGIF;
+  CGIF_Config     gConfig;
+  CGIF_FrameConfig   fConfig;
+  uint8_t*      pImageData;
+  uint8_t       aPalette[] = {
+    0x00, 0x00, 0x00, // black
+    0xFF, 0xFF, 0xFF, // white
+  };
+  cgif_result r;
+
+  memset(&gConfig, 0, sizeof(CGIF_Config));
+  memset(&fConfig, 0, sizeof(CGIF_FrameConfig));
+  gConfig.width                   = WIDTH;
+  gConfig.height                  = HEIGHT;
+  gConfig.pGlobalPalette          = aPalette;
+  gConfig.numGlobalPaletteEntries = 2;
+  gConfig.pWriteFn                = pWriteFn;
+  gConfig.pContext                = NULL;
+  //
+  // create new GIF
+  pGIF = cgif_newgif(&gConfig);
+  if(pGIF == NULL) {
+    fputs("failed to create new GIF via cgif_newgif()\n", stderr);
+    return 1;
+  }
+  //
+  // add frames to GIF
+  pImageData = malloc(WIDTH * HEIGHT);
+  memset(pImageData, 0, WIDTH * HEIGHT);
+  fConfig.pImageData = pImageData;
+  r = cgif_addframe(pGIF, &fConfig);
+  pImageData[2] = 32; // 32 is not a valid index in this case.
+  r = cgif_addframe(pGIF, &fConfig);
+  r = cgif_addframe(pGIF, &fConfig);
+  r = cgif_addframe(pGIF, &fConfig);
+  free(pImageData);
+  //
+  // write GIF to file
+  r = cgif_close(pGIF);                  // free allocated space at the end of the session
+
+  // check for correct error: CGIF_EINDEX
+  if(r == CGIF_EINDEX) {
+    return 0;
+  }
+  fputs("CGIF_EINDEX expected as result code\n", stderr);
+  return 2;
+}
